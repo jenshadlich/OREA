71af64cfcdd438ea4ec03687a0cd4fcf2a75ccee
jenshadlich <jenshadlich@googlemail.com>
2015-02-23 08:56:17 +0100

    bugfix

--- src/Genetic/Selection/RandomIndividuum.hs
+++ src/Genetic/Selection/RandomIndividuum.hs
@@ -5,12 +5,11 @@ import System.Random (randomRIO)
 import Genetic.Types
 
 --------------------------------------------------------------------------------
--- | Zufaellig (gleichverteilt) ein Individuum aus der Liste der Individuen
--- auswaehlen.
+-- | pick random individum from a list
 randomIndividuum :: [Individuum gt f]
                  -> IO (Individuum gt f)
 randomIndividuum is = do i <- pick is; return i
 
---| pick a random element from a list
+-- | pick a random element from a list
 pick :: [a] -> IO a
 pick xs = randomRIO (0, (length xs - 1)) >>= return . (xs !!)
