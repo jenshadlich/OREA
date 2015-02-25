ca932c0e950c6887a1db172bc80b4f4143524ee5
jenshadlich <jenshadlich@googlemail.com>
2015-02-25 23:29:15 +0100

    translate

--- src/Examples/BitStringImpl.hs
+++ src/Examples/BitStringImpl.hs
@@ -5,7 +5,7 @@ import Genetic.Types
 import qualified Util.Random as R
 
 --------------------------------------------------------------------------------
--- | Zufaelligen Bitstring mit einer max. Laenge erzeugen.
+-- | Generate a random bit string of maxLength
 generate :: Genpool Char -> Int -> IO (String)
 generate gp maxLen = do
     r <- R.getRandomNumber(1, maxLen)
@@ -14,16 +14,15 @@ generate gp maxLen = do
         where s = terminals gp
 
 --------------------------------------------------------------------------------
--- | Bewertung: Vergleich der Bitpositionen von Individuum und Ziel-Bitstring.
--- Ist 0 wenn alle Positionen und die Laenge uebereinstimmen.
+-- | Evalutate: compare bit positions of indivuum and target
+-- result is 0 if all positions are equal
 evaluate :: Genpool Char -> String -> String -> Double
 evaluate _ t i =
     fromInteger (toInteger (abs (length i - length t))) + 
     (sum $ map (\(x, y) -> if x == y then 0 else 1) (zip t i))
 
 --------------------------------------------------------------------------------
--- | Rekombination: 2 Bitstrings an zufaellingen Positionen auftrennen und
--- rekombinieren.
+-- | Crossover: Split and join at random positions
 crossover :: String -> String -> IO ([String])
 crossover m d = do 
     (mh, mt) <- splitRandom m
@@ -35,7 +34,7 @@ crossover m d = do
             return $ splitAt r a
             
 --------------------------------------------------------------------------------
--- Mutation: Fuegt ein zufaelliges Terminal an zufaelliger Position ein. 
+-- Mutation: insert a random terminal symbol at a random position
 mutate :: Genpool Char -> String -> IO (String)
 mutate gp i = do
     r <- R.getRandomNumber(0, length i)
@@ -44,7 +43,7 @@ mutate gp i = do
     return $ h ++ [c] ++ if length t > 0 then tail t else []
 
 --------------------------------------------------------------------------------
--- Zufaelliges Terminal auswaehlen.
+-- Chose one random character
 randomChar :: String -> IO (Char)
 randomChar s = do
     r <- R.getRandomNumber (1, length s)
